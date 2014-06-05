a77afb43101ff44552565f258d057a02203f0e60
Milo Casagrande <milo.casagrande@linaro.org>
2014-06-05 17:35:04 +0200

    builds.html: Move JavaScript code in external file.
    Change-Id: Ic81a620c7dec1e1ff67e6252fe86b1d2a334dc52

---
+++ app/dashboard/static/js/linaro-builds-1.0.js
@@ -0,0 +1,138 @@
+$(document).ready(function() {
+    $('#li-build').addClass('active');
+
+    $('body').tooltip({
+        'selector': '[rel=tooltip]',
+        'placement': 'auto'
+    });
+
+    $('#defconfstable').dataTable({
+        'dom': '<"row"<"col-lg-6"<"length-menu"l>>' +
+            '<"col-lg-4 col-lg-offset-2"f>r<"col-lg-12"t>>' +
+            '<"row"<"col-lg-6"i><"col-lg-6"p>>',
+        'language': {
+            'lengthMenu': '_MENU_&nbsp;<strong>builds per page</strong>',
+            'zeroRecords': '<strong>No builds to display.</strong>',
+            'search': '<div id="search-area" class="input-group"><span class="input-group-addon"><i class="fa fa-search"></i></span>_INPUT_</div>'
+        },
+        'lengthMenu': [25, 50, 75, 100],
+        'ordering': true,
+        'processing': true,
+        'stateDuration': -1,
+        'stateSave': true,
+        'order': [4, 'desc'],
+        'search': {
+            'regex': true
+        },
+        'ajax': {
+            'url': '/_ajax/defconf',
+            'traditional': true,
+            'dataType': 'json',
+            'dataSrc': 'result',
+            'dataFilter': function(data, type) {
+                if (type === 'json') {
+                    var parsed = JSON.parse(data);
+                    parsed.result = JSON.parse(parsed.result);
+                    return JSON.stringify(parsed);
+                }
+                return data;
+            },
+            'data': {
+                'sort': 'created_on',
+                'sort_order': -1,
+                'date_range': 15,
+                'field': [
+                    '_id', 'job', 'kernel', 'defconfig', 'status',
+                    'metadata', 'arch', 'created_on'
+                ]
+            }
+        },
+        'columns': [
+            {
+                'data': 'job',
+                'title': 'Tree &dash; Branch',
+                'render': function(data, type, object) {
+                    return data + '&nbsp;&dash;&nbsp;<small>' +
+                        object.metadata.git_branch + '</small>';
+                }
+            },
+            {
+                'data': 'kernel',
+                'title': 'Kernel'
+            },
+            {
+                'data': 'defconfig',
+                'title': 'Defconfig'
+            },
+            {
+                'data': 'arch',
+                'title': 'Arch.'
+            },
+            {
+                'data': 'created_on',
+                'title': 'Date',
+                'type': 'date',
+                'render': function(data, type, object) {
+                    var created = new Date(data['$date']);
+                    return created.getCustomISODate();
+                }
+            },
+            {
+                'data': 'status',
+                'title': 'Status',
+                'type': 'string',
+                'render': function(data, type, object) {
+                    var displ;
+                    switch (data) {
+                        case 'PASS':
+                            displ = '<span rel="tooltip" ' +
+                                'data-toggle="tooltip"' +
+                                'title="Build completed">' +
+                                '<span class="label label-success">' +
+                                '<li class="fa fa-check"></li></span></span>';
+                            break;
+                        case 'FAIL':
+                            displ = '<span rel="tooltip" ' +
+                                'data-toggle="tooltip"' +
+                                'title="Build failed">' +
+                                '<span class="label label-danger">' +
+                                '<li class="fa fa-exclamation-triangle">' +
+                                '</li></span></span>';
+                            break;
+                        default:
+                            displ = '<span rel="tooltip" ' +
+                                'data-toggle="tooltip"' +
+                                'title="Unknown status">' +
+                                '<span class="label label-warning">' +
+                                '<li class="fa fa-question">' +
+                                '</li></span></span>';
+                            break;
+                    }
+                    return displ;
+                }
+            },
+            {
+                'data': 'job',
+                'title': '',
+                'orderable': false,
+                'searchable': false,
+                'render': function(data, type, object) {
+                    return '<span rel="tooltip" data-toggle="tooltip"' +
+                        'title="Details for&nbsp;' + data +
+                        '&nbsp;&dash;&nbsp;' + object.kernel + '">' +
+                        '<a href="/job/' + data +
+                        '/kernel/' + object.kernel + '">' +
+                        '<i class="fa fa-search"></i></a></span>';
+                }
+            }
+        ]
+    });
+
+    $('#search-area > .input-sm').attr('placeholder', 'Filter the results');
+    $('.input-sm').keyup(function(key) {
+        // Remove focus from input when Esc is pressed.
+        if (key.keyCode === 27) {
+            $(this).blur();
+        }
+    });
+});
