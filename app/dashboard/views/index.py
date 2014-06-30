edc5afa0c4d194633994fe008be1fea73c9d0396
Milo Casagrande <milo.casagrande@linaro.org>
2014-06-30 17:33:04 +0200

    Index: Remove unused variables.
    Change-Id: Ib7bee9f60c012fb8a169910a288283efd12d3907

--- app/dashboard/views/index.py
+++ app/dashboard/views/index.py
@@ -14,7 +14,6 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from flask import render_template
-from flask import current_app as app
 from flask.views import View
 
 from dashboard.utils.backend import today_date
@@ -23,12 +22,10 @@ from dashboard.utils.backend import today_date
 class IndexView(View):
 
     def dispatch_request(self, *args, **kwargs):
-        base_url = app.config.get('BASE_URL') + "/job"
         page_title = 'Kernel CI Dashboad &mdash; Home'
 
         return render_template(
             'index.html',
             page_tile=page_title,
-            base_url=base_url,
             server_date=today_date(),
         )
