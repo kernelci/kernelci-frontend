98df037b66920e12c962bba4f2f43da2530de9c1
Milo Casagrande <milo.casagrande@linaro.org>
2014-12-17 12:38:57 +0100

    Use current_app in route initialization.
    Change-Id: I28dcd6ce595733c37bdd6a86cd9245cefda7d9b5

--- app/dashboard/utils/route.py
+++ app/dashboard/utils/route.py
@@ -11,6 +11,8 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from flask import current_app as app
+
 import dashboard.views.about as vabout
 import dashboard.views.boot as vboot
 import dashboard.views.build as vbuild
@@ -18,36 +20,39 @@ import dashboard.views.index as vindex
 import dashboard.views.job as vjob
 
 
-def init(app):
+def init():
+    # Save the function.
+    add_rule = app.add_url_rule
+
     # General URLs.
-    app.add_url_rule(
+    add_rule(
         "/",
         view_func=vindex.IndexView.as_view("index"),
         methods=["GET"]
     )
-    app.add_url_rule(
+    add_rule(
         "/info/",
         view_func=vabout.AboutView.as_view("about"),
         methods=["GET"]
     )
 
     # Builds related URLs.
-    app.add_url_rule(
+    add_rule(
         "/build/",
         view_func=vbuild.BuildsAllView.as_view("builds"),
         methods=["GET"],
     )
-    app.add_url_rule(
+    add_rule(
         "/build/all/",
         view_func=vbuild.BuildsAllView.as_view("all-builds"),
         methods=["GET"]
     )
-    app.add_url_rule(
+    add_rule(
         "/build/<string:job>/kernel/<string:kernel>/",
         view_func=vbuild.BuildsJobKernelView.as_view("job-kernel-builds"),
         methods=["GET"]
     )
-    app.add_url_rule(
+    add_rule(
         (
             "/build/<string:job>/kernel/<string:kernel>"
             "/defconfig/<string:defconfig>/"
@@ -56,36 +61,36 @@ def init(app):
             "job-kernel-defconf"),
         methods=["GET"]
     )
-    app.add_url_rule(
+    add_rule(
         "/build/<string:job>/",
         view_func=vjob.JobsJobView.as_view("build-job"),
         methods=["GET"]
     )
 
     # Jobs related URLs
-    app.add_url_rule(
+    add_rule(
         "/job/",
         view_func=vjob.JobsAllView.as_view("all-jobs"),
         methods=["GET"]
     )
-    app.add_url_rule(
+    add_rule(
         "/job/<string:job>/",
         view_func=vjob.JobsJobView.as_view("job-name"),
         methods=["GET"]
     )
 
     # Boots related URLs.
-    app.add_url_rule(
+    add_rule(
         "/boot/",
         view_func=vboot.BootAllView.as_view("boots"),
         methods=["GET"]
     )
-    app.add_url_rule(
+    add_rule(
         "/boot/all/",
         view_func=vboot.BootAllView.as_view("all-boots"),
         methods=["GET"]
     )
-    app.add_url_rule(
+    add_rule(
         (
             "/boot/<string:board>/job/<string:job>/kernel/<string:kernel>/"
             "defconfig/<string:defconfig>/"
@@ -93,7 +98,7 @@ def init(app):
         view_func=vboot.BootDefconfigView.as_view("boot-defconfig"),
         methods=["GET"]
     )
-    app.add_url_rule(
+    add_rule(
         (
             "/boot/<string:board>/job/<string:job>/kernel/<string:kernel>/"
             "defconfig/<string:defconfig>/lab/<string:lab_name>/"
@@ -101,33 +106,33 @@ def init(app):
         view_func=vboot.BootIdView.as_view("boot-id"),
         methods=["GET"]
     )
-    app.add_url_rule(
+    add_rule(
         "/boot/all/job/<string:job>/kernel/<string:kernel>/",
         view_func=vboot.BootJobKernelView.as_view("boot-job-kernel"),
         methods=["GET"]
     )
-    app.add_url_rule(
+    add_rule(
         "/boot/all/job/<string:job>/",
         view_func=vboot.BootJobView.as_view("boot-job"),
         methods=["GET"]
     )
-    app.add_url_rule(
+    add_rule(
         "/boot/all/lab/<string:lab_name>/",
         view_func=vboot.BootLab.as_view("boot-lab"),
         methods=["GET"]
     )
-    app.add_url_rule(
+    add_rule(
         "/boot/<string:board>/job/<string:job>/kernel/<string:kernel>/",
         view_func=vboot.BootBoardJobKernelView.as_view(
             "boot-board-job-kernel"),
         methods=["GET"]
     )
-    app.add_url_rule(
+    add_rule(
         "/boot/<string:board>/",
         view_func=vboot.BootBoardView.as_view("boot-board"),
         methods=["GET"]
     )
-    app.add_url_rule(
+    add_rule(
         "/boot/<string:board>/job/<string:job>/",
         view_func=vboot.BootBoardJobView.as_view("boot-board-job"),
         methods=["GET"]
