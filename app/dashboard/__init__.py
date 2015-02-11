c2fbb067d2c162d43278c8050cd0fd01e75c81e2
Milo Casagrande <milo.casagrande@linaro.org>
2015-02-11 16:34:49 +0800

    Bump app version number.
    Change-Id: I7f846a3ddffbcf9239fa2e8aa352d1b7a35c49f9

--- app/dashboard/__init__.py
+++ app/dashboard/__init__.py
@@ -28,7 +28,7 @@ from flask_wtf.csrf import (
 )
 from flask.ext.cache import Cache
 
-__version__ = "2015.1.2"
+__version__ = "2015.2"
 __versionfull__ = __version__
 
 CSRF_TOKEN_H = "X-Csrftoken"
