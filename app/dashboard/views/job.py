e975b27484a3d1a6cc96df604f99a4a3efab92b8
Milo Casagrande <milo.casagrande@linaro.org>
2015-06-26 17:33:05 +0200

    Add better title to job page.

--- app/dashboard/views/job.py
+++ app/dashboard/views/job.py
@@ -42,13 +42,15 @@ class JobsAllView(GeneralJobsView):
 
 class JobsJobView(GeneralJobsView):
     def dispatch_request(self, **kwargs):
-        body_title = "Details for&nbsp;&#171;%s&#187;" % kwargs["job"]
+        job = kwargs["job"]
+        body_title = "Details for&nbsp;&#171;%s&#187;" % job
+        page_title = "%s &mdash; &#171;%s&#187; job" % (self.PAGE_TITLE, job)
         search_filter, page_len = get_search_parameters(request)
         return render_template(
             "jobs-job.html",
             body_title=body_title,
-            job=kwargs["job"],
+            job=job,
             page_len=page_len,
-            page_title=self.PAGE_TITLE,
+            page_title=page_title,
             search_filter=search_filter
         )
