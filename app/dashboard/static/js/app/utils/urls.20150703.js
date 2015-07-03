e34bc77c51a213b9018e0b3eb1cecd0dd994ccb0
Milo Casagrande <milo.casagrande@linaro.org>
2015-07-03 10:17:41 +0200

    Use newly defined git rules for git url translation.

--- app/dashboard/static/js/app/utils/urls.20150703.js
+++ app/dashboard/static/js/app/utils/urls.20150703.js
@@ -13,8 +13,9 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 define([
-    'sprintf'
-], function(p) {
+    'sprintf',
+    'utils/git-rules'
+], function(p, gitRules) {
     'use strict';
     var urls = {};
 
@@ -44,50 +45,45 @@ define([
         return [tURI, tURIp];
     };
 
-    urls.translateCommit = function(url, sha, rules) {
+    urls.translateCommit = function(url, sha) {
         var bURL = null,
             cURL = null,
-            i = 0,
-            tRules,
+            idx = 0,
             parser,
             hostName,
             urlPath,
             knownGit,
             rule,
             lenRule;
-        if (arguments.length === 3) {
-            if ((url !== null && url !== '') && (sha !== null && sha !== '') &&
-                (rules !== null && rules !== '' && rules !== 'None'))
-            {
-                tRules = JSON.parse(rules);
-                parser = new URI(url);
-                hostName = parser.hostname();
-                urlPath = parser.path();
 
-                // Perform translation only if we know the host.
-                if (tRules.hasOwnProperty(hostName)) {
-                    knownGit = tRules[hostName];
-                    rule = knownGit[3];
-                    lenRule = rule.length;
+        if ((url !== null && url !== '') && (sha !== null && sha !== '')) {
+            parser = new URI(url);
+            hostName = parser.hostname();
+            urlPath = parser.path();
 
-                    for (i; i < lenRule; i = i + 1) {
-                        urlPath = urlPath.replace(rule[i][0], rule[i][1]);
-                    }
+            // Perform translation only if we know the host.
+            if (gitRules.hasOwnProperty(hostName)) {
+                knownGit = gitRules[hostName];
+                rule = knownGit[3];
+                lenRule = rule.length;
 
-                    bURL = new URI({
-                        protocol: knownGit[0],
-                        hostname: hostName,
-                        path: p.sprintf(knownGit[1], urlPath)
-                    });
-                    cURL = new URI({
-                        protocol: knownGit[0],
-                        hostname: hostName,
-                        path: p.sprintf(knownGit[2], urlPath) + sha
-                    });
-
-                    bURL = bURL.href();
-                    cURL = cURL.href();
+                for (idx; idx < lenRule; idx = idx + 1) {
+                    urlPath = urlPath.replace(rule[idx][0], rule[idx][1]);
                 }
+
+                bURL = new URI({
+                    protocol: knownGit[0],
+                    hostname: hostName,
+                    path: p.sprintf(knownGit[1], urlPath)
+                });
+                cURL = new URI({
+                    protocol: knownGit[0],
+                    hostname: hostName,
+                    path: p.sprintf(knownGit[2], urlPath) + sha
+                });
+
+                bURL = bURL.href();
+                cURL = cURL.href();
             }
         }
         return [bURL, cURL];
