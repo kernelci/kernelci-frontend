70f179d5ba8dee3849375e753cf08d9f0f217b3f
Milo Casagrande <milo.casagrande@linaro.org>
2015-07-10 10:21:25 +0200

    Bump app version number.

--- app/dashboard/__init__.py
+++ app/dashboard/__init__.py
@@ -30,7 +30,7 @@ from flask_wtf.csrf import (
 )
 from flask.ext.cache import Cache
 
-__version__ = "2015.7.2"
+__version__ = "2015.7.3"
 __versionfull__ = __version__
 
 CSRF_TOKEN_H = "X-Csrftoken"
