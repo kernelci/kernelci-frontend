b41de4745afe4af753a5a5f01901581f4c52b694
Milo Casagrande <milo.casagrande@linaro.org>
2015-08-27 10:10:59 +0200

    Fix version/license header.

--- app/dashboard/static/js/kci-builds-job-kernel-defconfig.js
+++ app/dashboard/static/js/kci-builds-job-kernel-defconfig.js
@@ -1,4 +1,4 @@
-/*! Kernel CI Dashboard v2015.8.3 | Licensed under the GNU GPL v3 (or later) */
+/*! Kernel CI Dashboard | Licensed under the GNU GPL v3 (or later) */
 require(['common'], function() {
     'use strict';
     require(['app/view-builds-job-kernel-defconfig']);
