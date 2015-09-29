691e5713876916264a6c10bb013c165d6bdb820e
Milo Casagrande <milo.casagrande@linaro.org>
2015-09-29 14:00:18 +0200

    Rework web storage logic.
      * Since we need to store and load more information for a single
        HTML node, expand the web storage logic to handle arrays of
        attributes.
      * Remove dependency on jQuery and swith to vanilla JavaScript.
      * Minor cleanups.

--- app/dashboard/static/js/app/utils/web-storage.js
+++ app/dashboard/static/js/app/utils/web-storage.js
@@ -1,73 +1,106 @@
 /*! Kernel CI Dashboard | Licensed under the GNU GPL v3 (or later) */
-define([
-    'jquery'
-], function($) {
+define(function() {
     'use strict';
     var storage = {};
 
-    function setClass(element, value) {
-        $(element).each(function() {
-            $(this).addClass(value);
-        });
+    function setClass(selector, value) {
+        [].forEach.call(
+            document.querySelectorAll(selector),
+            function(element) {
+                element.className = value;
+            }
+        );
     }
 
-    function setAttr(element, attribute, value) {
-        $(element).each(function() {
-            $(this).attr(attribute, value);
-        });
+    function setAttr(selector, attribute, value) {
+        [].forEach.call(
+            document.querySelectorAll(selector),
+            function(element) {
+                element.setAttribute(attribute, value);
+            }
+        );
     }
 
     function toJson(objects) {
-        var json = {};
-        if (arguments.length) {
+        var json;
+
+        json = {};
+        if (objects !== null && objects !== undefined) {
             json = JSON.stringify(objects);
         }
+
         return json;
     }
 
+    function loadSessionData(key, data) {
+        var loaded;
+        loaded = false;
+
+        if (data.value !== null && data.value !== '') {
+            switch (data.type) {
+                case 'attr':
+                    loaded = loaded || true;
+                    setAttr(key, data.name, data.value);
+                    break;
+                case 'class':
+                    loaded = loaded || true;
+                    setClass(key, data.value);
+                    break;
+                default:
+                    loaded = false;
+                    break;
+            }
+        }
+
+        return loaded;
+    }
+
+    function parseSessionData(key, data) {
+        var loaded,
+            eachLoaded;
+
+        loaded = false;
+        if (data.constructor === Array) {
+            data.forEach(function(value) {
+                eachLoaded = loadSessionData(key, value);
+                loaded = loaded || eachLoaded;
+            });
+        } else {
+            loaded = loadSessionData(key, data);
+        }
+
+        return loaded;
+    }
+
     storage.Session = function(name) {
-        var that = this;
-        if (arguments.length) {
+        var that;
+
+        that = this;
+        if (name !== null && name !== undefined) {
             that.name = name;
             that.objects = null;
         }
     };
 
     storage.load = function(name) {
-        var loaded = false,
+        var loaded,
             sessionItem,
-            sessionState = {},
-            key,
-            data;
+            sessionState;
+
+        loaded = false;
         if (window.Storage) {
-            if (arguments.length) {
+            if (name !== null && name !== undefined) {
+                sessionState = {};
                 sessionItem = sessionStorage.getItem(name);
                 if (sessionItem) {
                     sessionState = JSON.parse(sessionItem);
-                    for (key in sessionState) {
-                        if (sessionState.hasOwnProperty(key)) {
-                            data = sessionState[key];
-                            if (data.value !== null && data.value !== '') {
-                                switch (data.type) {
-                                    case 'attr':
-                                        loaded = loaded || true;
-                                        setAttr(key, data.name, data.value);
-                                        break;
-                                    case 'class':
-                                        loaded = loaded || true;
-                                        setClass(key, data.value);
-                                        break;
-                                    default:
-                                        loaded = loaded && false;
-                                        break;
-                                }
-                            }
-                        }
-                    }
+                    Object.keys(sessionState).forEach(function(key) {
+                        loaded = parseSessionData(key, sessionState[key]);
+                    });
                 }
             }
         } else {
-            console.warn('Loading from session/local storage is not supported');
+            console.warn('Loading from session storage is not supported');
         }
         return loaded;
     };
