c7d4e075edde51905db9e21dd729b84bb5c558ea
Milo Casagrande <milo.casagrande@linaro.org>
2015-10-21 18:07:50 +0200

    Add new app constants definition
      * Add a new constants definition file for the app.

---
+++ app/dashboard/static/js/app/utils/const.js
@@ -0,0 +1,15 @@
+/*! Kernel CI Dashboard | Licensed under the GNU GPL v3 (or later) */
+define(function() {
+    'use strict';
+    var appConstants;
+
+    appConstants = {};
+
+    var MAX_DATE_RANGE = 14,
+        MAX_QUERY_LIMIT = 1024;
+
+    appConstants,MAX_DATE_RANGE = MAX_DATE_RANGE;
+    appConstants.MAX_QUERY_LIMIT = MAX_QUERY_LIMIT;
+
+    return appConstants;
+});
