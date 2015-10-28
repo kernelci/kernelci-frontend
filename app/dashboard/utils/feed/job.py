13e210273ded0caf531c044e89cb4babdf4103a5
Milo Casagrande <milo.casagrande@linaro.org>
2015-10-28 16:35:13 +0100

    Rework job feed
      * Add boots and builds count to the feed.
      * Rework the feed entry title.
      * Remove forgotten print statement.

--- app/dashboard/utils/feed/job.py
+++ app/dashboard/utils/feed/job.py
@@ -11,6 +11,12 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+try:
+    import simple_json as json
+except ImportError:
+    import json
+
+import copy
 import hashlib
 
 import werkzeug.contrib.atom as watom
@@ -20,8 +26,8 @@ from flask import (
     request
 )
 
-import dashboard.utils.feed as feed
 import dashboard.utils.backend as backend
+import dashboard.utils.feed as feed
 
 from dashboard import __version__
 
@@ -30,21 +36,58 @@ CONFIG_GET = app.config.get
 # Necessary URLs for the feed.
 BACKEND_JOB_URL = backend.create_url(CONFIG_GET("JOB_API_ENDPOINT"))
 
-FRONTEND_JOB_URL = CONFIG_GET("BASE_URL") + "/job/%(job)s/kernel/%(kernel)s/"
-FRONTEND_JOB_BRANCH_URL = (
-    CONFIG_GET("BASE_URL") +
-    "/job/%(job)s/kernel/%(kernel)s/branch/%(branch)s/")
+FRONTEND_JOB_KERNEL_URL = \
+    CONFIG_GET("BASE_URL") + "/job/%(job)s/kernel/%(kernel)s/"
+FRONTEND_JOB_URL = CONFIG_GET("BASE_URL") + "/job/%(job)s/"
+FRONTEND_JOB_BRANCH_URL = \
+    CONFIG_GET("BASE_URL") + "/job/%(job)s/branch/%(branch)s/"
+
+FEED_TITLE = u"Built kernel \u00AB%(kernel)s\u00BB (%(git_branch)s)"
 
 JOB_CONTENT_TEMPLATE = u"""
 <div>
+<div>
+<h3>Overview</h3>
 <dl>
 <dt>Tree</dt><dd>%(job)s</dd>
 <dt>Kernel</dt><dd>%(kernel)s</dd>
 <dt>Git branch</dt><dd>%(git_branch)s</dd>
 <dt>Git URL</dt><dd>%(git_url)s</dd>
 <dt>Git commit</dt><dd>%(git_commit)s</dd>
-<dt>Status</dt><dd>%(status)s</dd>
 </dl>
+</div>
+<div>
+<h3>Build and Boot Reports Count</h3>
+<h4>Build Reports</h4>
+<div>
+Total: %(total_builds)s
+<br/>
+Passed: %(passed_builds)s
+<br/>
+Failed: %(failed_builds)s
+<br/>
+Other: %(other_builds)s
+</div>
+<h4>Boot Reports</h4>
+<div>
+Total: %(total_boots)s
+<br/>
+Passed: %(passed_boots)s
+<br/>
+Failed: %(failed_boots)s
+<br/>
+Other: %(other_boots)s
+</div>
+</div>
+<div>
+<h3>Links</h3>
+<div>
+<ul>
+<li><a href="/build/%(job)s/kernel/%(kernel)s/">Build reports</a></li>
+<li><a href="/boot/all/job/%(job)s/kernel/%(kernel)s/">Boot reports</a></li>
+</ul>
+</div>
+</div>
 </div>
 """
 
@@ -54,6 +97,143 @@ FEED_CATEGORIES = [
     {"term": "ci"},
     {"term": "linux"}
 ]
+
+
+def _parse_batch_results(results):
+    """Parse the batch results.
+
+    :param results: The results from the prev query.
+    :type results: dict
+    :return: A dictionary with the counts.
+    """
+    count_results = {
+        "failed_boots": u"N/A",
+        "failed_builds": u"N/A",
+        "other_boots": u"N/A",
+        "other_builds": u"N/A",
+        "passed_boots": u"N/A",
+        "passed_builds": u"N/A",
+        "total_boots": u"N/A",
+        "total_builds": u"N/A"
+    }
+
+    if results and results["result"]:
+        results = results["result"]
+
+        for result in results:
+            count_results[result["operation_id"]] = \
+                result["result"][0]["count"]
+
+        count_results["other_builds"] = (
+            count_results["total_builds"] -
+            count_results["passed_builds"] - count_results["failed_builds"]
+        )
+
+        count_results["other_boots"] = (
+            count_results["total_boots"] -
+            count_results["passed_boots"] - count_results["failed_boots"]
+        )
+
+    return count_results
+
+
+def _get_job_counts(query_params):
+    """Retrieve the builds and boots count.
+
+    :param query_params: The result from the previous query to be used as
+    parameter.
+    :type query_params: dict
+    :return: A dictionary with the buils and boots count.
+    """
+    queries = []
+
+    query_params["job_id"] = query_params["_id"]["$oid"]
+    query_str = "status=%(status)s&job=%(job)s&kernel=%(kernel)s"
+    total_query_str = "job=%(job)s&kernel=%(kernel)s" % query_params
+
+    # Get the totals.
+    queries.append({
+        "method": "GET",
+        "operation_id": "total_builds",
+        "resource": "count",
+        "document": "build",
+        "query": total_query_str
+    })
+    queries.append({
+        "method": "GET",
+        "operation_id": "total_boots",
+        "resource": "count",
+        "document": "boot",
+        "query": total_query_str
+    })
+
+    # Get the passed ones first.
+    query_params["status"] = "PASS"
+    queries.append(
+        {
+            "method": "GET",
+            "operation_id": "passed_builds",
+            "resource": "count",
+            "document": "build",
+            "query": query_str % query_params
+        }
+    )
+
+    queries.append(
+        {
+            "method": "GET",
+            "operation_id": "passed_boots",
+            "resource": "count",
+            "document": "boot",
+            "query": query_str % query_params
+        }
+    )
+
+    # Then the failed ones.
+    query_params["status"] = "FAIL"
+    queries.append(
+        {
+            "method": "GET",
+            "operation_id": "failed_builds",
+            "resource": "count",
+            "document": "build",
+            "query": query_str % query_params
+        }
+    )
+
+    queries.append(
+        {
+            "method": "GET",
+            "operation_id": "failed_boots",
+            "resource": "count",
+            "document": "boot",
+            "query": query_str % query_params
+        }
+    )
+
+    data, status, headers = backend.request_post(
+        backend.create_url(CONFIG_GET("BATCH_API_ENDPOINT")),
+        json.dumps({"batch": queries}),
+        headers={"Content-Type": "application/json"},
+        timeout=60*60
+    )
+
+    if status == 200:
+        count_results = _parse_batch_results(
+            backend.extract_gzip_data(data, headers))
+    else:
+        count_results = {
+            "failed_boots": u"N/A",
+            "failed_builds": u"N/A",
+            "other_boots": u"N/A",
+            "other_builds": u"N/A",
+            "passed_boots": u"N/A",
+            "passed_builds": u"N/A",
+            "total_boots": u"N/A",
+            "total_builds": u"N/A"
+        }
+
+    return count_results
 
 
 def _parse_job_results(results, branch=None):
@@ -67,24 +247,25 @@ def _parse_job_results(results, branch=None):
     :return: Yield a dictionary with the correct values for the feed entry.
     """
     for result in results:
-        job_link = FRONTEND_JOB_URL
+        counts = _get_job_counts(copy.deepcopy(result))
+        result.update(counts)
 
+        alternate_link = FRONTEND_JOB_URL
         if branch:
-            job_link = FRONTEND_JOB_BRANCH_URL
+            alternate_link = FRONTEND_JOB_BRANCH_URL
             result["branch"] = branch[1]
 
-        job_link = job_link % result
         job_date = feed.convert_date(result["created_on"]["$date"])
 
         parsed_res = {
             "content": JOB_CONTENT_TEMPLATE % result,
             "links": [
-                {"href": job_link, "rel": "alternate"}
+                {"href": alternate_link, "rel": "alternate"}
             ],
             "published": job_date,
-            "title": u"%(job)s \u2013 %(kernel)s \u2014 %(status)s" % result,
+            "title": FEED_TITLE % result,
             "updated": job_date,
-            "url": job_link
+            "url": FRONTEND_JOB_KERNEL_URL % result
         }
 
         yield parsed_res
@@ -112,8 +293,7 @@ def _get_job_data(job, branch=None):
                 "git_commit",
                 "git_url",
                 "job",
-                "kernel",
-                "status"
+                "kernel"
             )
         ),
         ("job", job),
@@ -122,8 +302,6 @@ def _get_job_data(job, branch=None):
     ]
     if branch:
         params.append(("git_branch", branch[0]))
-
-    print params
 
     data, status, headers = backend.request_get(
         BACKEND_JOB_URL, params=params, timeout=60*60)
@@ -197,7 +375,7 @@ def job_branch_feed(job, branch):
     branch = [branch.replace(":", "/", 1), branch]
 
     title = (
-        u"kernelci.org \u2014 Jobs for Tree: \u00AB%s\u00BB (%s)" %
+        u"kernelci.org \u2014 Jobs for Tree \u00AB%s\u00BB (%s)" %
         (job, branch[0])
     )
     subtitle = u"Latest available jobs for %s \u2013 %s" % (job, branch[0])
@@ -210,7 +388,7 @@ def job_feed(job):
     """Create the Atom feed for job views."""
     feed_url = request.url
     cache_key = hashlib.md5(feed_url).digest()
-    title = u"kernelci.org \u2014 Jobs for Tree: \u00AB%s\u00BB" % job
+    title = u"kernelci.org \u2014 Jobs for Tree \u00AB%s\u00BB" % job
     subtitle = u"Latest available jobs for %s" % job
 
     return _create_job_feed(job, cache_key, title, subtitle, feed_url)
