f95fb28a6ef81a49b17cf308e9cea7c69850d9e9
Milo Casagrande <milo.casagrande@linaro.org>
2015-11-02 16:23:00 +0100

    Add new const value

--- app/dashboard/static/js/app/utils/const.js
+++ app/dashboard/static/js/app/utils/const.js
@@ -4,6 +4,7 @@ define(function() {
     var appConstants;
 
     appConstants = {
+        MAX_COMPARE_LIMIT: 3,
         MAX_DATE_RANGE: 14,
         MAX_NUMBER_RANGE: 20,
         MAX_QUERY_LIMIT: 1024
