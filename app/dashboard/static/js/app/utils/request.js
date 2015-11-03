dbed59a6cefa6bee22cba62efb2d60c88a26a2e0
Milo Casagrande <milo.casagrande@linaro.org>
2015-11-03 17:25:02 +0100

    Clean up code on request creation

--- app/dashboard/static/js/app/utils/request.js
+++ app/dashboard/static/js/app/utils/request.js
@@ -3,48 +3,49 @@ define([
     'jquery'
 ], function($) {
     'use strict';
-    var get,
-        post,
-        request,
-        settings,
-        token = null;
+    var request,
+        settings;
 
-    if (document.querySelector('meta[name=csrf-token]') !== null) {
-        token = document.querySelector('meta[name=csrf-token]').content;
-    }
-
+    request = {};
     settings = {
         traditional: true,
         cache: true,
         dataType: 'json',
         timeout: 35000,
         beforeSend: function(jqXHR) {
-            jqXHR.setRequestHeader('X-CSRFToken', token);
+            jqXHR.setRequestHeader('X-CSRFToken', _getToken());
         }
     };
 
-    get = function(url, data) {
-        return request('GET', url, data);
-    };
+    function _getToken() {
+        var token;
 
-    post = function(url, data) {
-        return request('POST', url, data);
-    };
+        token = null;
+        if (document.querySelector('meta[name=csrf-token]') !== null) {
+            token = document.querySelector('meta[name=csrf-token]').content;
+        }
+        return token;
+    }
 
-    request = function(method, url, data) {
+    function _makeRequest(method, url, data) {
         settings.headers = {'Content-Type': 'application/json'};
         settings.type = method;
 
-        if (data === null || data === undefined) {
+        if (!data) {
             data = {};
         }
         settings.data = data;
 
         return $.ajax(url, settings);
+    }
+
+    request.get = function(url, data) {
+        return _makeRequest('GET', url, data);
     };
 
-    return {
-        get: get,
-        post: post
+    request.post = function(url, data) {
+        return _makeRequest('POST', url, data);
     };
+
+    return request;
 });
