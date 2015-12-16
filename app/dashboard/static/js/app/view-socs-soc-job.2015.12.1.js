c00a0266e75b53d6b197ad368a23da576178f392
Milo Casagrande <milo.casagrande@linaro.org>
2015-12-16 17:41:39 +0100

    Add boot total/other count to SoC-tree view

--- app/dashboard/static/js/app/view-socs-soc-job.2015.12.1.js
+++ app/dashboard/static/js/app/view-socs-soc-job.2015.12.1.js
@@ -10,12 +10,21 @@ require([
     'tables/soc'
 ], function($, init, format, html, table, request, error, tsoc) {
     'use strict';
-    var gBootsTable,
+    var gBatchCountMissing,
+        gBootsTable,
         gJob,
         gNumberRange,
         gQueryStr,
         gSearchFilter,
-        gSoc;
+        gSoc,
+        gTableCount;
+
+    document.getElementById('li-soc').setAttribute('class', 'active');
+    init.hotkeys();
+    init.tooltip();
+
+    gTableCount = {};
+    gBatchCountMissing = {};
 
     function updateCountDetail(result) {
         html.replaceContent(
@@ -73,6 +82,29 @@ require([
             .done(getDetailsDone);
     }
 
+    function updateOrStageCount(elementId, count) {
+        var element;
+
+        element = document.getElementById(elementId);
+        // If we do not have the element in the DOM, it means dataTables has
+        // yet to add it.
+        if (element) {
+            html.replaceContent(
+                element, document.createTextNode(format.number(count)));
+
+            // Check if the data structure holding the data to update the
+            // elements still holds the element.
+            if (gBatchCountMissing.hasOwnProperty(elementId)) {
+                delete gBatchCountMissing[elementId];
+            }
+        } else {
+            // Store it in a dictionary for later access.
+            if (!gBatchCountMissing.hasOwnProperty(elementId)) {
+                gBatchCountMissing[elementId] = count;
+            }
+        }
+    }
+
     function getBootsCountFail() {
         html.replaceByClassHTML('count-badge', '&infin;');
     }
@@ -80,43 +112,76 @@ require([
     function getBootsCountDone(response) {
         var results;
 
-        results = response.result;
+        function _parseOperationsResult(result) {
+            gTableCount[result.operation_id] =
+                parseInt(result.result[0].count, 10);
+        }
 
-        if (results.length === 0) {
-            html.replaceByClassTxt('count-badge', '?');
+        function _updateTable(opId) {
+            updateOrStageCount(opId, gTableCount[opId]);
+        }
+
+        results = response.result;
+        if (results.length > 0) {
+            // Parse all the results and update a global object with
+            // the operation IDs and the count found.
+            results.forEach(_parseOperationsResult);
+            // Invalidate the cells in column #3 before updating the DOM
+            // elements. In this way we have the correct 'filter' values in the
+            // global object that we can use to provide the search parameters.
+            gBootsTable.invalidateColumn(3);
+            // Now update the DOM with the results.
+            Object.keys(gTableCount).forEach(_updateTable);
+
+            gBootsTable
+                .search(gSearchFilter);
         } else {
-            results.forEach(updateCountDetail);
+            html.replaceByClassTxt('count-badge', '?');
         }
     }
 
     function getBootsCount(response) {
         var batchOps,
             deferred,
-            failStr,
             kernel,
-            results,
-            successStr;
+            results;
 
         batchOps = [];
-        successStr = gQueryStr + '&status=PASS';
-        failStr = gQueryStr + '&status=FAIL';
 
         function _prepareBatchOps(result) {
             kernel = result.kernel;
+
             batchOps.push({
-                operation_id: 'success-count-' + kernel,
+                operation_id: 'boot-total-count-' + kernel,
                 method: 'GET',
                 resource: 'count',
                 document: 'boot',
-                query: successStr + '&kernel=' + kernel
+                query: gQueryStr + '&kernel=' + kernel
             });
 
             batchOps.push({
-                operation_id: 'fail-count-' + kernel,
+                operation_id: 'boot-success-count-' + kernel,
                 method: 'GET',
                 resource: 'count',
                 document: 'boot',
-                query: failStr + '&kernel=' + kernel
+                query: gQueryStr + '&status=PASS&kernel=' + kernel
+            });
+
+            batchOps.push({
+                operation_id: 'boot-fail-count-' + kernel,
+                method: 'GET',
+                resource: 'count',
+                document: 'boot',
+                query: gQueryStr + '&status=FAIL&kernel=' + kernel
+            });
+
+            batchOps.push({
+                operation_id: 'boot-unknown-count-' + kernel,
+                method: 'GET',
+                resource: 'count',
+                document: 'boot',
+                query: gQueryStr + '&staus=OFFLINE&status=UNKNOWN&kernel=' +
+                    kernel
             });
         }
 
@@ -140,23 +205,57 @@ require([
             html.errorDiv('Error loading data.'));
     }
 
+    function getFilterBootCount(data) {
+        var filter;
+
+        filter = '';
+        if (gTableCount.hasOwnProperty('boot-success-count-' + data)) {
+            if (gTableCount['boot-success-count-' + data]) {
+                filter += 'successfulpass';
+            }
+        }
+
+        if (gTableCount.hasOwnProperty('boot-fail-count-' + data)) {
+            if (gTableCount['boot-fail-count-' + data]) {
+                filter += 'failed';
+            }
+        }
+
+        return filter;
+    }
+
     function getBootsDone(response) {
         var columns,
             results;
 
-        // Internal wrapper.
+        // Internal wrapper for the filter.
         function _renderBootCount(data, type) {
-            return tsoc.countSuccessFail({
-                data: data,
-                type: type,
-                extraClasses: ['extra-margin']
-            });
+            if (type === 'filter') {
+                return getFilterBootCount(data);
+            } else {
+                return tsoc.renderBootCount(data, type);
+            }
         }
 
         // Internal wrapper to provide the href.
         function _renderDetails(data, type) {
             return tsoc.renderDetails(
                 '/soc/' + gSoc + '/job/' + gJob + '/kernel/' + data, type);
+        }
+
+        /**
+         * Create the table column title for the boots count.
+        **/
+        function _bootColumnTitle() {
+            var tooltipNode;
+
+            tooltipNode = html.tooltip();
+            tooltipNode.setAttribute(
+                'title', 'Total/Successful/Failed/Other boot reports');
+            tooltipNode.appendChild(
+                document.createTextNode('Boot Results'));
+
+            return tooltipNode.outerHTML;
         }
 
         results = response.result;
@@ -170,22 +269,25 @@ require([
                 {
                     data: 'kernel',
                     title: 'Kernel',
+                    type: 'string',
                     className: 'kernel-column'
                 },
                 {
                     data: 'git_branch',
                     title: 'Branch',
+                    type: 'string',
                     className: 'branch-column'
                 },
                 {
                     data: 'git_commit',
                     title: 'Commit',
+                    type: 'string',
                     className: 'commit-column'
                 },
                 {
                     data: 'kernel',
-                    title: 'Boot Status',
-                    searchable: false,
+                    title: _bootColumnTitle(),
+                    type: 'string',
                     orderable: false,
                     className: 'boot-count pull-center',
                     render: _renderBootCount
@@ -193,12 +295,14 @@ require([
                 {
                     data: 'created_on',
                     title: 'Date',
+                    type: 'date',
                     className: 'date-column pull-center',
                     render: tsoc.renderDate
                 },
                 {
                     data: 'kernel',
                     title: '',
+                    type: 'string',
                     searchable: false,
                     orderable: false,
                     className: 'select-column pull-center',
@@ -216,9 +320,6 @@ require([
                 .rowURLElements(['mach', 'job', 'kernel'])
                 .order([4, 'desc'])
                 .draw();
-
-            gBootsTable
-                .search(gSearchFilter);
         }
     }
 
@@ -251,10 +352,6 @@ require([
             .fail(error.error, getBootsFail)
             .done(getBootsDone, getBootsCount);
     }
-
-    document.getElementById('li-soc').setAttribute('class', 'active');
-    init.hotkeys();
-    init.tooltip();
 
     if (document.getElementById('job-name') !== null) {
         gJob = document.getElementById('job-name').value;
