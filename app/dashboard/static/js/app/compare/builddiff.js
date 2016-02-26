bf7b266e29c01bb708cf02c4f2b8628e269ff7ee
Milo Casagrande <milo.casagrande@linaro.org>
2016-02-26 17:11:07 +0100

    Add build comparison resources

---
+++ app/dashboard/static/js/app/compare/builddiff.js
@@ -0,0 +1,452 @@
+/*! Kernel CI Dashboard | Licensed under the GNU GPL v3 (or later) */
+define([
+    'jquery',
+    'utils/html',
+    'utils/format',
+    'charts/diffmatrix'
+], function($, html, format, diffmatrix) {
+    'use strict';
+    var gBuildDiff;
+    var yData;
+
+    gBuildDiff = {};
+
+    function isValidData(val) {
+        var isValid;
+        var toCheck;
+
+        isValid = false;
+        toCheck = String(val).toLowerCase();
+
+        if (val !== null && val !== undefined) {
+            if (isFinite(val) || !isNaN(val) || toCheck.length > 0) {
+                if (toCheck !== 'null' &&
+                        toCheck !== 'undefined' && toCheck !== 'none') {
+                    isValid = true;
+                }
+            }
+        }
+
+        return isValid;
+    }
+
+    function isNumber(number) {
+        if (number !== null &&
+                number !== undefined && !isNaN(number) && isFinite(number)) {
+            return true;
+        }
+        return false;
+    }
+
+    function renderText(args) {
+        var text;
+
+        text = document.createElement('text');
+        text.setAttribute('text-anchor', 'middle');
+        text.setAttribute('transform', 'translate(0,4)');
+        text.setAttribute('x', args.chart.square().width / 2);
+        text.setAttribute('y', args.chart.square().height / 2);
+        text.className = 'diff-text';
+
+        text.appendChild(document.createTextNode(args.value));
+
+        return text;
+    }
+
+    function renderTextLong(args) {
+        var rendered;
+        var old;
+
+        if (args.value.length > 14) {
+            old = args.value;
+            args.value = html.sliceText(args.value, 14);
+
+            rendered = renderText(args);
+
+            $(args.parent.parentNode).tooltip({
+                html: true,
+                trigger: 'hover',
+                container: 'body',
+                placement: 'top',
+                title: old
+            });
+        } else {
+            rendered = renderText(args);
+        }
+
+        return rendered;
+    }
+
+    function renderStatus(args) {
+        var rect;
+        var status;
+        var tooltip;
+
+        status = args.value.toLowerCase();
+
+        rect = document.createElement('rect');
+        rect.setAttribute(
+            'x',
+            (args.chart.square().width / 2) - (diffmatrix.status.width / 2));
+        rect.setAttribute(
+            'y',
+            (args.chart.square().height / 2) - (diffmatrix.status.height / 2));
+        rect.setAttribute('rx', 2.5);
+        rect.setAttribute('ry', 2.5);
+        rect.setAttribute('width', diffmatrix.status.width);
+        rect.setAttribute('height', diffmatrix.status.height);
+
+        if (diffmatrix.colors.hasOwnProperty(status)) {
+            rect.setAttribute('fill', diffmatrix.colors[status]);
+        } else {
+            rect.setAttribute('fill', diffmatrix.colors.unavail);
+        }
+
+        if (diffmatrix.build_status.hasOwnProperty(status)) {
+            tooltip = diffmatrix.build_status[status];
+        } else {
+            tooltip = diffmatrix.build_status.unavail;
+        }
+
+        $(args.parent).tooltip({
+            html: true,
+            trigger: 'hover',
+            container: 'body',
+            placement: 'top',
+            title: tooltip
+        });
+
+        return rect;
+    }
+
+    function renderFormat(args) {
+        var diff;
+        var diffNumber;
+        var height;
+        var text;
+        var tspan;
+        var width;
+
+        if (!args.format) {
+            args.format = function(value) {
+                return value;
+            };
+        }
+
+        width = args.chart.square().width;
+        height = args.chart.square().height;
+
+        text = document.createElement('text');
+        text.setAttribute('text-anchor', 'middle');
+        text.setAttribute('transform', 'translate(0,4)');
+        text.setAttribute('x', width / 2);
+        text.setAttribute('y', height / 2);
+        text.className = 'diff-text';
+
+        tspan = document.createElement('tspan');
+        tspan.setAttribute('x', width / 2);
+
+        tspan.appendChild(document.createTextNode(args.format(args.value)));
+        text.appendChild(tspan);
+
+        if (args.valueIdx !== 0) {
+            if (isNumber(args.baseNumber) && isNumber(args.value)) {
+                diffNumber = args.value - args.baseNumber;
+
+                // Reset the text container position and y-translation.
+                text.setAttribute('y', height / 4);
+                text.setAttribute('transform', 'translate(0,10)');
+
+                tspan = document.createElement('tspan');
+                tspan.setAttribute('x', width / 2);
+                tspan.setAttribute('dy', '1.2em');
+
+                if (diffNumber === 0) {
+                    diff = '(' + diffmatrix.symbols.plus_minus +
+                        args.format(diffNumber) + ')';
+                } else if (diffNumber > 0) {
+                    diff = '(' + diffmatrix.symbols.plus +
+                        args.format(diffNumber) + ')';
+                } else {
+                    diff = '(' + diffmatrix.symbols.minus +
+                        args.format(Math.abs(diffNumber)) + ')';
+                }
+
+                tspan.innerHTML = diff;
+                text.appendChild(tspan);
+            }
+        }
+
+        return text;
+    }
+
+    function renderNumber(args) {
+        args.value = parseInt(args.value, 10);
+
+        if (args.baseline.hasOwnProperty(args.key)) {
+            args.baseNumber = parseInt(args.baseline[args.key], 10);
+        } else {
+            args.baseNumber = 0;
+        }
+
+        args.format = format.number;
+
+        return renderFormat(args);
+    }
+
+    function renderBytes(args) {
+        args.value = parseInt(args.value, 10);
+
+        if (args.baseline.hasOwnProperty(args.key)) {
+            args.baseNumber = parseInt(args.baseline[args.key], 10);
+        } else {
+            args.baseNumber = Math.NaN;
+        }
+
+        args.format = format.bytes;
+
+        return renderFormat(args);
+    }
+
+    function renderBuildTime(args) {
+        args.value = parseFloat(args.value);
+
+        if (args.baseline.hasOwnProperty(args.key)) {
+            args.baseNumber = parseFloat(args.baseline[args.key]);
+        } else {
+            args.baseNumber = Math.NaN;
+        }
+
+        args.format = format.number;
+
+        return renderFormat(args);
+    }
+
+    function renderCompiler(args) {
+        var compiler;
+        var rendered;
+
+        compiler = args.datum.compiler_version_full;
+
+        args.parent
+            .parentNode.setAttribute('data-compiler', compiler);
+
+        $(args.parent.parentNode).tooltip({
+            html: true,
+            trigger: 'hover',
+            container: 'body',
+            placement: 'top',
+            title: compiler
+        });
+
+        rendered = renderText(args);
+
+        return rendered;
+    }
+
+    // Contains mappings between JSON data keys, labels for display and
+    // render function. If the render function is null the default text render
+    // will be used.
+    // This is used to provide the y-axis labels, along with the keys to use
+    // to retrieve data from the binded JSON data.
+    yData = [
+        ['job', 'Tree', null],
+        ['kernel', 'Kernel', renderTextLong],
+        ['arch', 'Architecture', null],
+        ['status', 'Status', renderStatus],
+        ['errors', 'Errors', renderNumber],
+        ['warnings', 'Warnings', renderNumber],
+        ['mismatches', 'Mistmatches', renderNumber],
+        ['compiler_version_ext', 'Compiler / Version', renderCompiler],
+        ['text_offset', 'Text offset', null],
+        ['build_time', 'Build time', renderBuildTime],
+        ['build_log_size', 'Build log size', renderBytes],
+        ['kernel_config_size', 'Config file size', renderBytes],
+        ['dtb_dir_data', 'Number of dtb files', renderNumber],
+        ['modules_size', 'Modules file size', renderBytes],
+        ['system_map_size', 'System.map file size', renderBytes]
+    ];
+
+    function setupMatrix(chart, data) {
+        var yDataIdx = {};
+
+        function matrixHead(datum, idx) {
+            var text;
+            var tspan;
+
+            text = document.createElement('text');
+            tspan = document.createElement('tspan');
+
+            tspan.appendChild(document.createTextNode(datum.defconfig));
+            tspan.setAttribute('y', chart.xScale(idx));
+            tspan.setAttribute('x', '0');
+            text.appendChild(tspan);
+
+            if (datum.defconfig !== datum.defconfig_full) {
+                tspan = document.createElement('tspan');
+                tspan.appendChild(
+                    document.createTextNode(datum.defconfig_full));
+                tspan.setAttribute('y', chart.xScale(idx));
+                tspan.setAttribute('x', '0');
+                tspan.setAttribute('dy', '1.2em');
+                tspan.className = 'diff-text-head-other';
+
+                text.appendChild(tspan);
+            }
+
+            return text.innerHTML;
+        }
+
+        function diffValues(datum, idx) {
+            var args;
+            var attrIdx;
+            var func;
+            var key;
+            var rendered;
+
+            key = this.getAttribute('data-key');
+            func = yData[yDataIdx[key]][2];
+            attrIdx = parseInt(this.getAttribute('data-idx'), 10);
+
+            args = {
+                attrIdx: attrIdx,
+                baseline: data.xdata[0],
+                chart: chart,
+                datum: datum,
+                key: key,
+                value: datum[key],
+                valueIdx: idx,
+                parent: this
+            };
+
+            if (isValidData(datum[key])) {
+                if (func) {
+                    rendered = func(args).outerHTML;
+                } else {
+                    rendered = renderText(args).outerHTML;
+                }
+            } else {
+                args.value = diffmatrix.icons.default;
+                rendered = renderText(args);
+                html.addClass(rendered, 'font-awe');
+
+                $(this.parentNode).tooltip({
+                    html: true,
+                    trigger: 'hover',
+                    container: 'body',
+                    placement: 'top',
+                    title: 'Not available'
+                });
+
+                rendered = rendered.outerHTML;
+            }
+
+            return rendered;
+        }
+
+        chart.matrixCells()
+            .append('rect')
+            .attr('width', chart.square().width)
+            .attr('height', chart.square().height)
+            .attr('rx', chart.square().rx)
+            .attr('class', 'matrix-background');
+
+        chart.xTextGroup()
+            .selectAll('text')
+            .data(data.xdata)
+            .enter()
+            .append('text')
+            .html(matrixHead)
+            .attr('y', function(ignore, idx) {
+                return chart.xScale(idx);
+            })
+            .attr('transform', 'rotate(-90) translate(6)')
+            .attr('class', 'diff-text-head');
+
+        chart.yTextGroup()
+            .selectAll('text')
+            .data(data.ydata)
+            .enter()
+            .append('text')
+            .text(function(datum, idx) {
+                // Map keys to their index for later use.
+                yDataIdx[datum[0]] = idx;
+                return datum[1];
+            })
+            .attr('y', function(ignore, idx) {
+                var yDx = chart.yScale(idx);
+                if (isNaN(yDx)) {
+                    yDx = chart.square().height / 2;
+                }
+                return yDx;
+            })
+            .attr('transform', 'translate(0,4)');
+
+        chart.matrixLines()
+            .selectAll('g.matrix-cell-group')
+            .attr('transform', function(ignore, idx) {
+                return 'translate(' +
+                    (chart.square().width * idx) + ',0)';
+            })
+            .append('g')
+            .attr('data-key', function(datum) {
+                return datum;
+            })
+            .attr('data-idx', function(ignore, idx) {
+                return idx;
+            })
+            .attr('class', 'diff-values');
+
+        chart.matrixLines()
+            .selectAll('g.diff-values')
+            .data(data.xdata)
+            .html(diffValues);
+    }
+
+    gBuildDiff.create = function(element, data) {
+        var args;
+        var i;
+        var newData;
+        var tData;
+        var xLen;
+
+        newData = [];
+        xLen = data.xdata.length;
+
+        function _cellDataFunc(ignore, idx) {
+            return newData[idx];
+        }
+
+        // Create the data structure used to populate the matrix based
+        // solely on the keys from the JSON data.
+        yData.forEach(function(ydata) {
+            i = xLen;
+            tData = [];
+
+            while (i > 0) {
+                tData.push(ydata[0]);
+                i = i - 1;
+            }
+
+            newData.push(tData);
+        });
+
+        // Inject the yData here since we handle it in a very special way.
+        data.ydata = yData;
+
+        args = {
+            element: element,
+            data: data,
+            setupFunc: setupMatrix,
+            cellDataFunc: _cellDataFunc,
+            options: {
+                square: {width: 95, height: 45, rx: 0},
+                groupPosition: {top: 180, right: 0, bottom: 2, left: 150}
+            }
+        };
+
+        diffmatrix.create(args);
+    };
+
+    return gBuildDiff;
+});
---
+++ app/dashboard/static/js/app/view-build-compare.2016.2.1.js
@@ -0,0 +1,372 @@
+/*! Kernel CI Dashboard | Licensed under the GNU GPL v3 (or later) */
+require([
+    'jquery',
+    'utils/init',
+    'utils/request',
+    'utils/html',
+    'utils/urls',
+    'utils/table',
+    'tables/build',
+    'compare/builddiff'
+], function($, init, request, html, urls, table, buildt, matrix) {
+    'use strict';
+    var gCompareId;
+    var gComparedTable;
+
+    document.getElementById('li-compare').setAttribute('class', 'active');
+
+    function setupComparedTable(comparedData) {
+        var columns;
+        var dom;
+
+        /**
+         * Wrapper to provide the href.
+        **/
+        function _renderKernel(data, type, object) {
+            return buildt.renderKernel(
+                data, type, '/job/' + object.job + '/kernel/' + data + '/');
+        }
+
+        /**
+         * Wrapper to provide the href.
+        **/
+        function _renderDetails(data, type, object) {
+            return buildt.renderDetails(
+                '/build/' + data + '/kernel/' + object.kernel +
+                '/defconfig/' + object.defconfig_full +
+                '/?_id=' + object._id.$oid,
+                type
+            );
+        }
+
+        /**
+         * Wrapper to provide the href.
+        **/
+        function _renderCommit(data, type, object) {
+            var aNode;
+            var rendered;
+            var textNode;
+            var tooltipNode;
+            var gitURL;
+
+            rendered = data;
+            if (type === 'display') {
+                gitURL = urls.translateCommit(object.git_url, data);
+                tooltipNode = html.tooltip();
+                tooltipNode.setAttribute('title', data);
+
+                if (gitURL[1] !== null) {
+                    aNode = document.createElement('a');
+                    aNode.setAttribute('href', gitURL[1]);
+
+                    textNode = document.createTextNode(data);
+                    aNode.appendChild(textNode);
+
+                    tooltipNode.appendChild(aNode);
+                } else {
+                    textNode = document.createTextNode(data);
+                    tooltipNode.appendChild(textNode);
+                }
+
+                rendered = tooltipNode.outerHTML;
+                // Remove the nodes.
+                textNode.remove();
+                if (aNode) {
+                    aNode.remove();
+                }
+                tooltipNode.remove();
+            }
+
+            return rendered;
+        }
+
+        dom = '<"row"' +
+            '<"col-xs-12 col-sm-12 col-md-12 col-lg-12"t>>';
+
+        columns = [
+            {
+                data: 'job',
+                title: 'Tree',
+                type: 'string',
+                className: 'tree-column',
+                render: buildt.renderTree
+            },
+            {
+                data: 'git_branch',
+                title: 'Branch',
+                type: 'string',
+                className: 'branch-column'
+            },
+            {
+                data: 'kernel',
+                title: 'Kernel',
+                type: 'string',
+                className: 'kernel-column',
+                render: _renderKernel
+            },
+            {
+                data: 'git_commit',
+                title: 'Commit',
+                type: 'string',
+                render: _renderCommit
+            },
+            {
+                data: 'created_on',
+                title: 'Date',
+                type: 'date',
+                className: 'date-column pull-center',
+                render: buildt.renderDate
+            },
+            {
+                data: 'job',
+                title: '',
+                type: 'string',
+                orderable: false,
+                searchable: false,
+                className: 'pull-center',
+                render: _renderDetails
+            }
+        ];
+
+        gComparedTable
+            .dom(dom)
+            .data(comparedData)
+            .columns(columns)
+            .order([5, 'desc'])
+            .noIdURL(true)
+            .rowURLElements(['job', 'kernel'])
+            .draw();
+    }
+
+    function getCompareFail() {
+        html.replaceByClassNode('loading-content', html.nonavail().outerHTML);
+    }
+
+    function setupBaselineData(baseline) {
+        var aNode;
+        var baseDefconfig;
+        var baseGitCommit;
+        var baseGitUrl;
+        var baseJob;
+        var baseKernel;
+        var gitURLs;
+        var spanNode;
+        var tooltipNode;
+
+        baseDefconfig = baseline.defconfig_full;
+        baseGitCommit = baseline.git_commit;
+        baseGitUrl = baseline.git_url;
+        baseJob = baseline.job;
+        baseKernel = baseline.kernel;
+
+        gitURLs = urls.translateCommit(baseGitUrl, baseGitCommit);
+
+        // Tree.
+        spanNode = document.createElement('span');
+
+        tooltipNode = html.tooltip();
+        tooltipNode.setAttribute('title', 'Details for tree&nbsp;' + baseJob);
+
+        aNode = document.createElement('a');
+        aNode.setAttribute('href', '/job/' + baseJob + '/');
+        aNode.appendChild(document.createTextNode(baseJob));
+
+        tooltipNode.appendChild(aNode);
+        spanNode.appendChild(tooltipNode);
+
+        tooltipNode = html.tooltip();
+        tooltipNode.setAttribute(
+            'title', 'Boot reports for tree&nbsp;' + baseJob);
+
+        aNode = document.createElement('a');
+        aNode.setAttribute('href', '/boot/all/job/' + baseJob + '/');
+        aNode.appendChild(html.boot());
+
+        tooltipNode.appendChild(aNode);
+
+        spanNode.insertAdjacentHTML('beforeend', '&nbsp;&mdash;&nbsp;');
+        spanNode.appendChild(tooltipNode);
+
+        html.replaceContent(document.getElementById('dd-tree'), spanNode);
+
+        // Git branch.
+        html.replaceContent(
+            document.getElementById('dd-branch'),
+            document.createTextNode(baseline.git_branch));
+
+        // Kernel.
+        spanNode = document.createElement('span');
+
+        tooltipNode = html.tooltip();
+        tooltipNode.setAttribute(
+            'title',
+            'Build details for&nbsp;' + baseJob +
+            '&nbsp;&dash;&nbsp;' + baseKernel
+        );
+
+        aNode = document.createElement('a');
+        aNode.setAttribute(
+            'href', '/build/' + baseJob + '/kernel/' + baseKernel + '/');
+        aNode.appendChild(document.createTextNode(baseKernel));
+
+        tooltipNode.appendChild(aNode);
+        spanNode.appendChild(tooltipNode);
+
+        tooltipNode = html.tooltip();
+        tooltipNode.setAttribute(
+            'title',
+            'Boot reports for&nbsp;' + baseJob +
+            '&nbsp;&dash;&nbsp;' + baseKernel
+        );
+
+        aNode = document.createElement('a');
+        aNode.setAttribute(
+            'href',
+            '/boot/all/job/' + baseJob + '/kernel/' + baseKernel + '/');
+        aNode.appendChild(html.boot());
+
+        tooltipNode.appendChild(aNode);
+        spanNode.insertAdjacentHTML('beforeend', '&nbsp;&mdash;&nbsp;');
+        spanNode.appendChild(tooltipNode);
+
+        html.replaceContent(document.getElementById('dd-kernel'), spanNode);
+
+        // Defconfig.
+        spanNode = document.createElement('span');
+
+        aNode = document.createElement('a');
+        aNode.setAttribute(
+            'href',
+            '/build/' + baseJob + '/kernel/' + baseKernel + '/defconfig/' +
+            baseDefconfig + '/?_id=' + baseline._id.$oid
+        );
+        aNode.appendChild(document.createTextNode(baseDefconfig));
+
+        spanNode.appendChild(aNode);
+
+        tooltipNode = html.tooltip();
+        tooltipNode.setAttribute(
+            'title',
+            'Boot reports for&nbsp;' + baseJob +
+                '&nbsp;&dash;&nbsp;' + baseKernel +
+                '&nbsp;&dash;&nbsp;' + baseDefconfig
+            );
+
+        aNode = document.createElement('a');
+        aNode.setAttribute(
+            'href',
+            '/boot/all/job/' + baseJob + '/kernel/' +
+            baseKernel + '/defconfig/' + baseDefconfig + '/'
+        );
+        aNode.appendChild(html.boot());
+
+        tooltipNode.appendChild(aNode);
+        spanNode.insertAdjacentHTML('beforeend', '&nbsp;&mdash;&nbsp;');
+        spanNode.appendChild(tooltipNode);
+
+        html.replaceContent(
+            document.getElementById('dd-defconfig'), spanNode);
+
+        // Git URL/commit.
+        if (gitURLs[0] !== null) {
+            aNode = document.createElement('a');
+            aNode.setAttribute('href', gitURLs[0]);
+            aNode.appendChild(document.createTextNode(baseGitUrl));
+            aNode.insertAdjacentHTML('beforeend', '&nbsp;');
+            aNode.appendChild(html.external());
+
+            html.replaceContent(
+                document.getElementById('dd-url'), aNode);
+        } else {
+            if (baseGitUrl !== null) {
+                html.replaceContent(
+                    document.getElementById('dd-url'),
+                    document.createTextNode(baseGitUrl));
+            } else {
+                html.replaceContent(
+                    document.getElementById('dd-url'), html.nonavail());
+            }
+        }
+
+        if (gitURLs[1] !== null) {
+            aNode = document.createElement('a');
+            aNode.setAttribute('href', gitURLs[1]);
+            aNode.appendChild(document.createTextNode(baseGitCommit));
+            aNode.insertAdjacentHTML('beforeend', '&nbsp;');
+            aNode.appendChild(html.external());
+
+            html.replaceContent(
+                document.getElementById('dd-commit'), aNode);
+        } else {
+            if (baseGitCommit !== null) {
+                html.replaceContent(
+                    document.getElementById('dd-commit'),
+                    document.createTextNode(baseGitCommit));
+            } else {
+                html.replaceContent(
+                    document.getElementById('dd-commit'),
+                    html.nonavail());
+            }
+        }
+
+        // Date.
+        html.replaceContent(
+            document.getElementById('dd-date'),
+            html.time(baseline.created_on));
+    }
+
+    function getCompareDone(response) {
+        var baseline;
+        var comparedData;
+        var matrixData;
+        var results;
+        var titleNode;
+
+        results = response.result;
+        if (results.length > 0) {
+            results = results[0];
+            baseline = results.baseline;
+            comparedData = results.compare_to;
+
+            titleNode = document.getElementById('body-title');
+            titleNode.insertAdjacentHTML(
+                'beforeend',
+                'for &#171;' + baseline.job +
+                '&#187;&nbsp;&dash;&nbsp;' + baseline.kernel +
+                '&nbsp;<small>(' + baseline.defconfig + ')</small>'
+            );
+
+            setupBaselineData(baseline);
+            setupComparedTable(comparedData);
+
+            matrixData = {xdata: [baseline]};
+            Array.prototype.push.apply(matrixData.xdata, comparedData);
+            matrix.create('diff-matrix', matrixData);
+        } else {
+            html.replaceByClassNode(
+                'loading-content', html.nonavail().outerHTML);
+        }
+    }
+
+    function getBuildCompare() {
+        var deferred;
+
+        deferred = request.get('/_ajax/build/compare/' + gCompareId + '/', {});
+        $.when(deferred)
+            .fail(getCompareFail)
+            .done(getCompareDone);
+    }
+
+    if (document.getElementById('compare-id') !== null) {
+        gCompareId = document.getElementById('compare-id').value;
+    }
+
+    gComparedTable = table({
+        tableId: 'compared-against'
+    });
+
+    getBuildCompare();
+
+    init.hotkeys();
+    init.tooltip();
+});
