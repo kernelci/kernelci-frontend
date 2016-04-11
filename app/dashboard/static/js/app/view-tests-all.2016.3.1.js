40fb8525806f6af450340d4373baf5fe0d01320f
Milo Casagrande <milo.casagrande@linaro.org>
2016-04-11 15:09:54 +0200

    Fix tests all view

--- app/dashboard/static/js/app/view-tests-all.2016.3.1.js
+++ app/dashboard/static/js/app/view-tests-all.2016.3.1.js
@@ -9,19 +9,54 @@ require([
 ], function($, init, request, err, html) {
     'use strict';
     var gSuitePrefix;
-    var gTestList;
+    var gTestsList;
 
     document.getElementById('li-test').setAttribute('class', 'active');
     // Prefix used to access the DOM elements.
     gSuitePrefix = 'suite-';
 
-    function getTestsFail(suite, response) {
-        // TODO
+    function getTestCasesFail(suite) {
+        Array.prototype.forEach.call(
+            suite.querySelectorAll('span.test-cases'),
+            function(element) {
+                element.innerHTML = '&infin;';
+            }
+        );
+    }
+
+    function getTestCasesDone(suite, response) {
+        var results;
+
+        results = response.result;
+        if (results.length > 0) {
+            results.forEach(function(result) {
+                html.replaceContent(
+                    suite.querySelector('#' + result.operation_id),
+                    document.createTextNode(result.result[0].count));
+            });
+        } else {
+            Array.prototype.forEach.call(
+                suite.querySelectorAll('span.test-cases'),
+                function(element) {
+                    html.replaceContent(element, document.createTextNode('?'));
+                }
+            );
+        }
+    }
+
+    function getTestsFail(suite) {
+        html.replaceContent(
+            document.getElementById(gSuitePrefix + suite),
+            html.errorDiv('Error loading data.'));
     }
 
     function getTestsDone(suite, response) {
+        var batchOps;
+        var deferred;
         var domElement;
+        var iNode;
         var results;
+        var spanNode;
         var tableBody;
         var tableCell;
         var tableHead;
@@ -35,7 +70,7 @@ require([
 
         results = response.result;
 
-        function _createTableRows(result) {
+        function _createTableRows(result, idx) {
             tableRow = tableBody.insertRow(-1);
 
             // Tree.
@@ -70,23 +105,44 @@ require([
 
             // Test sets.
             tableCell = tableRow.insertCell();
+            tableCell.className = 'pull-center';
+
+            spanNode = document.createElement('span');
+            spanNode.id = 'test-sets-' + suite + idx;
+            spanNode.className = 'badge count-badge test-sets';
+
             if (result.test_set) {
-                tableCell.appendChild(
+                spanNode.appendChild(
                     document.createTextNode(result.test_set.length));
             } else {
-                tableCell.appendChild(document.createTextNode('0'));
+                spanNode.appendChild(document.createTextNode('0'));
             }
-            tableCell.className = 'pull-center';
+
+            tableCell.appendChild(spanNode);
 
             // Test cases.
             tableCell = tableRow.insertCell();
-            if (result.test_case) {
-                tableCell.appendChild(
-                    document.createTextNode(result.test_case.length));
-            } else {
-                tableCell.appendChild(document.createTextNode('0'));
-            }
             tableCell.className = 'pull-center';
+
+            spanNode = document.createElement('span');
+            spanNode.id = 'test-cases-' + suite + idx;
+            spanNode.className = 'badge count-badge test-cases';
+
+            iNode = document.createElement('i');
+            iNode.className = 'fa fa-cog fa-spin';
+
+            spanNode.appendChild(iNode);
+            tableCell.appendChild(spanNode);
+
+            // Complete the batch operation for each test suite id found to
+            // calculate its test cases.
+            batchOps.push({
+                method: 'GET',
+                resource: 'count',
+                document: 'test_case',
+                operation_id: 'test-cases-' + suite + idx,
+                query: 'test_suite_id=' + result._id.$oid
+            });
 
             // Date.
             tableCell = tableRow.insertCell();
@@ -98,16 +154,13 @@ require([
             // TODO
             // Details link.
             tableCell = tableRow.insertCell();
-            var tooltipNode = html.tooltip();
-            tooltipNode.setAttribute('data-title', 'More info');
-            var aNode = document.createElement('a');
-            aNode.setAttribute('href', '/test/' + suite + '/');
-            tooltipNode.appendChild()
             tableCell.innerHTML = '&nbsp;';
             tableCell.className = 'select-column pull-center';
         }
 
         if (results.length > 0) {
+            // Initialize the batch operations.
+            batchOps = [];
             tableNode = document.createElement('table');
 
             tableNode = document.createElement('table');
@@ -154,14 +207,28 @@ require([
 
             tableNode.appendChild(tableBody);
             html.replaceContent(domElement, tableNode);
+
+            // If we have some batch operations, perform them now.
+            if (batchOps.length > 0) {
+                deferred = request.post(
+                    '/_ajax/batch', JSON.stringify({batch: batchOps}));
+
+                $.when(deferred)
+                    .fail(err.error, function() {
+                        getTestCasesFail(domElement);
+                    })
+                    .done(function(data) {
+                        getTestCasesDone(domElement, data);
+                    });
+            }
         } else {
             html.replaceContent(domElement, html.errorDiv('No data found.'));
         }
     }
 
     function getTests() {
-        var deferred;
         var data;
+        var deferred;
 
         data = {
             sort: 'created_on',
@@ -169,7 +236,7 @@ require([
             limit: '5'
         };
 
-        function _createRequest(suite) {
+        function getData(suite) {
             data.name = suite;
 
             deferred = request.get('/_ajax/test/suite', data);
@@ -177,20 +244,19 @@ require([
                 .done(function(response) {
                     getTestsDone(suite, response);
                 })
-                .fail(err.error, function(response) {
-                    getTestsFail(suite, response);
+                .fail(err.error, function() {
+                    getTestsFail(suite);
                 });
         }
 
-        gTestList.forEach(_createRequest);
+        gTestsList.forEach(getData);
     }
 
-    document.getElementById('li-test').setAttribute('class', 'active');
-    gTestList = document.getElementById('test-suite-names');
-    if (gTestList !== null) {
-        gTestList = String(gTestList.value).split(',');
+    gTestsList = document.getElementById('test-suite-names');
+    if (gTestsList !== null) {
+        gTestsList = String(gTestsList.value).split(',');
 
-        if (gTestList.length > 0) {
+        if (gTestsList.length > 0) {
             getTests();
         }
     }
