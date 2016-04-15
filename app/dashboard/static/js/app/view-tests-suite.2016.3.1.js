c1c7ade89a7434d80d3cdac710eb99f62935d54c
Milo Casagrande <milo.casagrande@linaro.org>
2016-04-15 11:20:35 +0200

    Add initial batch operations to retrieve data

--- app/dashboard/static/js/app/view-tests-suite.2016.3.1.js
+++ app/dashboard/static/js/app/view-tests-suite.2016.3.1.js
@@ -1,5 +1,87 @@
 /*! Kernel CI Dashboard | Licensed under the GNU GPL v3 (or later) */
 require([
-], function() {
+    'jquery',
+    'utils/init',
+    'utils/html',
+    'utils/error',
+    'utils/request'
+], function($, init, html, err, request) {
     'use strict';
+    var gTestSuite;
+
+    document.getElementById('li-test').setAttribute('class', 'active');
+
+    function getCountsFail() {
+        html.replaceByClass('count-list-badge', '&infin;');
+    }
+
+    function getCountsDone(response) {
+        console.log(response);
+    }
+
+    function getCounts() {
+        var batchOps;
+        var queryStr;
+        var deferred;
+
+        batchOps = [];
+        queryStr = 'test_suite_name=' + gTestSuite;
+
+        batchOps.push({
+            method: 'GET',
+            resource: 'count',
+            document: 'test_set',
+            operation_id: 'count-test-sets',
+            query: queryStr
+        });
+
+        batchOps.push({
+            method: 'GET',
+            resource: 'count',
+            document: 'test_case',
+            operation_id: 'count-test-cases',
+            query: queryStr
+        });
+
+        batchOps.push({
+            method: 'GET',
+            resource: 'test_suite',
+            distinct: 'build_id',
+            operation_id: 'count-builds',
+            query: queryStr
+        });
+
+        batchOps.push({
+            method: 'GET',
+            resource: 'test_suite',
+            distinct: 'boot_id',
+            operation_id: 'count-boots',
+            query: queryStr
+        });
+
+        batchOps.push({
+            method: 'GET',
+            resource: 'test_suite',
+            distinct: 'board',
+            operation_id: 'count-boards',
+            query: queryStr
+        });
+
+        deferred = request.post(
+            '/_ajax/batch', JSON.stringify({batch: batchOps}));
+
+        $.when(deferred)
+            .done(getCountsDone)
+            .fail(err.error, getCountsFail);
+    }
+
+    gTestSuite = document.getElementById('test-suite');
+    if (gTestSuite) {
+        gTestSuite = gTestSuite.value;
+
+        getCounts();
+    }
+
+    init.hotkeys();
+    init.tooltip();
 });
