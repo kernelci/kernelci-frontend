666dffddd913abdc2b23c087161ec7a612abbc11
Milo Casagrande <milo.casagrande@linaro.org>
2016-04-29 11:46:40 +0200

    Bump app to 2016.4.1

--- app/dashboard/__init__.py
+++ app/dashboard/__init__.py
@@ -30,7 +30,7 @@ from flask_wtf.csrf import (
 )
 from flask.ext.cache import Cache
 
-__version__ = "2016.4"
+__version__ = "2016.4.1"
 __versionfull__ = __version__
 
 CSRF_TOKEN_H = "X-Csrftoken"
