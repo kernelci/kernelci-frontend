1170d5dce3d4edbf58721a0f835e6a186a044f72
Milo Casagrande <milo.casagrande@linaro.org>
2016-06-10 16:45:48 +0200

    Bump module version number

--- app/dashboard/static/js/common.js
+++ app/dashboard/static/js/common.js
@@ -38,7 +38,7 @@ require.config({
             'app/view-boots-job-kernel': 'app/view-boots-job-kernel.2016.6',
             'app/view-build-compare': 'app/view-build-compare.2016.6',
             'app/view-builds-all': 'app/view-builds-all.2016.6',
-            'app/view-builds-id': 'app/view-builds-id.2016.6',
+            'app/view-builds-id': 'app/view-builds-id.2016.6.1',
             'app/view-builds-job-kernel': 'app/view-builds-job-kernel.2016.6',
             'app/view-builds-job-kernel-defconfig': 'app/view-builds-job-kernel-defconfig.2016.6',
             'app/view-builds-job-kernel-defconfig-logs': 'app/view-builds-job-kernel-defconfig-logs.2016.6',
