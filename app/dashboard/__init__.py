c091423ea2aefbb9ceab86cad00d27f37b06df1b
Ana Guerrero Lopez <ana.guerrero@collabora.com>
2018-09-14 17:49:30 +0000

    Rename JS files and bump app version to 2018.9
    Signed-off-by: Ana Guerrero Lopez <ana.guerrero@collabora.com>

--- app/dashboard/__init__.py
+++ app/dashboard/__init__.py
@@ -32,7 +32,7 @@ from flask_wtf.csrf import (
 from flask_cache import Cache
 from werkzeug.routing import BaseConverter
 
-__version__ = "2018.6"
+__version__ = "2018.9"
 __versionfull__ = __version__
 
 CSRF_TOKEN_H = "X-Csrftoken"
