36f56cace068183ed14098a4a5b27e919a768ba6
Guillaume Tucker <guillaume.tucker@collabora.com>
2020-01-28 22:52:32 +0000

    Add passpie.testpie
    Add pie chart variant for test results passpie.testpie, with a title
    set to "test results".  This will be used by test result views.
    
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

--- app/dashboard/static/js/app/charts/passpie.js
+++ app/dashboard/static/js/app/charts/passpie.js
@@ -1,8 +1,8 @@
 /*!
  * kernelci dashboard.
- * 
+ *
  * Copyright (C) 2014, 2015, 2016, 2017  Linaro Ltd.
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 2.1 of the License, or (at your option)
@@ -119,6 +119,11 @@ define([
 
     passpie.bootpie = function(settings) {
         settings.text = 'total boots';
+        prepareGraph(settings);
+    };
+
+    passpie.testpie = function(settings) {
+        settings.text = 'test results';
         prepareGraph(settings);
     };
 
