83e6627196590e7deb469be0e021a4347b47afda
Alexandra Pereira <alexandra.pereira@collabora.com>
2020-03-12 13:58:06 -0300

    remove view boot all job
    Now kernel-ci front-end has test results views
    implemented, so the legacy boot views can be removed.
    This PR removes boot-all-job links and views.
    
    Signed-off-by: Alexandra Pereira <alexandra.pereira@collabora.com>

--- app/dashboard/static/js/common.js
+++ app/dashboard/static/js/common.js
@@ -59,7 +59,6 @@ require.config({
         '*': {
             'app/view-boots-all': 'app/view-boots-all.2017.5',
             'app/view-boots-all-jb': 'app/view-boots-all-jb.2017.3.4',
-            'app/view-boots-all-job': 'app/view-boots-all-job.2017.3.4',
             'app/view-boots-all-job-branch-kernel': 'app/view-boots-all-job-branch-kernel.2017.4',
             'app/view-boots-all-job-branch-kernel-defconfig': 'app/view-boots-all-job-branch-kernel-defconfig.2017.3.3',
             'app/view-boots-all-job-kernel-defconfig': 'app/view-boots-all-job-kernel-defconfig.2017.3.3',
