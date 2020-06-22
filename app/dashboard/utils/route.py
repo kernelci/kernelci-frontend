5783a1ec457680b8a0cf6f5e269048127817115f
Guillaume Tucker <guillaume.tucker@collabora.com>
2020-06-22 14:19:26 +0100

    Remove view-boots-all-job-kernel-defconfig
    Remove the view-boots-all-job-kernel-defconfig view and all related
    files.
    
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

--- app/dashboard/utils/route.py
+++ app/dashboard/utils/route.py
@@ -182,14 +182,6 @@ def init():
     )
     add_rule(
         (
-            "/boot/all/job/<string:job>/branch/<string:branch>/"
-            "kernel/<string:kernel>/defconfig/<string:defconfig>/"
-        ),
-        view_func=vboot.BootAllJBKDView.as_view("boot-all-jbkd"),
-        methods=["GET"]
-    )
-    add_rule(
-        (
             "/boot/<string:board>/job/<string:job>/kernel/<string:kernel>/"
             "defconfig/<string:defconfig>/"
         ),
