83b45a44f8924c15db9fb66eb3f3551f85403930
Guillaume Tucker <guillaume.tucker@collabora.com>
2020-06-22 14:05:40 +0100

    Remove view-boots-all-job-branch-kernel-defconfig
    Remove the view-boots-all-job-branch-kernel-defconfig view and all
    associated files.
    
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

--- app/dashboard/views/boot.py
+++ app/dashboard/views/boot.py
@@ -55,50 +55,6 @@ class BootAllView(BootGeneralView):
         )
 
 
-class BootAllJobKernelDefconfigView(BootGeneralView):
-    def dispatch_request(self, **kwargs):
-
-        page_title = self.BOOT_PAGES_TITLE + ""
-        body_title = (
-            "Boot Reports: &#171;{job} &ndash; {kernel}&#187;&nbsp;"
-            "<small>({defconfig})</small>".format(**kwargs)
-        )
-        search_filter, page_len = get_search_parameters(request)
-
-        return render_template(
-            "boots-all-job-kernel-defconfig.html",
-            page_title=page_title,
-            body_title=body_title,
-            job=kwargs["job"],
-            kernel=kwargs["kernel"],
-            defconfig=kwargs["defconfig"],
-            search_filter=search_filter,
-            page_len=page_len
-        )
-
-
-class BootAllJBKDView(BootGeneralView):
-    def dispatch_request(self, **kwargs):
-
-        page_title = self.BOOT_PAGES_TITLE + ""
-        body_title = (
-            "Boot Reports: &#171;{job} &ndash; {kernel}&#187;&nbsp;"
-            "<small>({branch} &ndash; {defconfig})</small>".format(**kwargs)
-        )
-        search_filter, page_len = get_search_parameters(request)
-
-        return render_template(
-            "boots-all-job-branch-kernel-defconfig.html",
-            page_title=page_title,
-            body_title=body_title,
-            job=kwargs["job"],
-            branch=kwargs["branch"],
-            kernel=kwargs["kernel"],
-            defconfig=kwargs["defconfig"],
-            search_filter=search_filter,
-            page_len=page_len
-        )
-
 class BootDefconfigView(BootGeneralView):
 
     def dispatch_request(self, **kwargs):
