a87f3661996c979e9a63d73f6f4d798f30cbdd8d
Guillaume Tucker <guillaume.tucker@collabora.com>
2020-09-10 12:02:49 +0100

    Remove unused functions in table/test.js
    Following the refactoring of test views, a number of functions in
    table/test.js aren't used any more so they can now be removed.
    
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

--- app/dashboard/static/js/app/tables/test.js
+++ app/dashboard/static/js/app/tables/test.js
@@ -36,30 +36,6 @@ define([
         return tcommon.statusNode(status, gStatusDefaults);
     };
 
-    /**
-     * Function to render the case detail.
-     *
-     * @param {string} link: The href link to point to.
-     * @return {Element} The DOM element.
-    **/
-    gTestTable.detailsNode = function(link) {
-        var aNode;
-        var str;
-        var tooltipNode;
-
-        tooltipNode = html.tooltip();
-        tooltipNode.setAttribute('title', 'More info');
-
-        aNode = document.createElement('a');
-        str = link;
-        aNode.setAttribute('href', str);
-
-        aNode.appendChild(html.search());
-        tooltipNode.appendChild(aNode);
-
-        return tooltipNode;
-    };
-
     gTestTable.renderDate = function(date, type) {
         return tcommon.renderDate(date, type);
     };
@@ -68,35 +44,8 @@ define([
         return tcommon.renderDetails(href, type, title);
     };
 
-    gTestTable.countBadge = function(settings) {
-        return tcommon.countBadge(
-            settings.data,
-            settings.type, settings.extraClasses, settings.idStart).outerHTML;
-    };
-
-    gTestTable.renderCasesCount = function(data, type, id_str, href) {
-        return tcommon.countAll({
-            data: data,
-            type: type,
-            href: href,
-            extraClasses: ['extra-margin'],
-            idStart: id_str
-        });
-    };
-
     gTestTable.renderTree = function(tree, type, href) {
         return tcommon.renderTree(tree, type, href);
-    };
-
-    gTestTable.getCountFail = function(idStart) {
-        document.getElementById('cases-total-count-'+ idStart)
-            .innerHTML ='&infin;';
-        document.getElementById('cases-success-count-'+ idStart)
-            .innerHTML ='&infin;';
-        document.getElementById('cases-fail-count-'+ idStart)
-            .innerHTML ='&infin;';
-        document.getElementById('cases-unknown-count-'+ idStart)
-            .innerHTML ='&infin;';
     };
 
     gTestTable.renderTestCount = function(settings) {
