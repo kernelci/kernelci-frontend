kernelci/kernelci-frontend

commit 93f3cfa6cbda43f0d6b2c7c50a61d90539b95f80

Add rename-views.sh to automatically rename view files
Add rename-views.sh to find all the view files that have been changed
since the last release tag and rename them with a new suffix for the
current year-month.  Also update the build.js and common.js files with
the new view names.

Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

rename-views.sh
@@ -1,0 +1,38 @@
+#!/bin/bash
+
+set -e
+
+JS_PATH="app/dashboard/static/js/"
+
+ver="$1"
+
+[ -z "$ver" ] && {
+    ver=$(date +%Y.%m)
+}
+
+echo "New version: $ver"
+
+since=$(git tag -l | grep -v staging | tail -1)
+echo "Changes since $since:"
+
+git diff --stat $since "$JS_PATH"app/view-*.js \
+    | grep -v "files changed" \
+    | cut -d\  -f2 \
+    | while read view
+do
+    old=$(basename $view .js)
+    name=$(echo $old | cut -d. -f1)
+    new="$name"."$ver"
+    echo "* $name"
+    echo "    old: $old"
+    echo "    new: $new"
+    for js in build.js common.js; do
+        sed -i s/$old/$new/g "$JS_PATH""$js"
+        git add "$JS_PATH""$js"
+    done
+    if [ $old != $new ]; then
+        git mv "$JS_PATH"/app/"$old".js "$JS_PATH"/app/"$new".js
+    fi
+done
+
+exit 0

commit 1518a538121a2b7074a01bda3bdd83dc1b6f49c4

Fix rename-views.sh when only one file changed
Fix the grep command in rename-views.sh when there was only one file
changed since the last release.

Fixes: 93f3cfa6cbda ("Add rename-views.sh to automatically rename view files")
Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

rename-views.sh
@@ -16,7 +16,7 @@ since=$(git tag -l | grep -v staging | tail -1)
 echo "Changes since $since:"
 
 git diff --stat $since "$JS_PATH"app/view-*.js \
-    | grep -v "files changed" \
+    | grep -v "changed," \
     | cut -d\  -f2 \
     | while read view
 do